262125c71b0e809ca31e36efffd14148e69c92e3
ubixx <ubitzer@t-online.de>
2026-04-15 11:47:39 +0200

    Create Sw.js

---
+++ Sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'budget-cloud-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
+];
+
+// Installieren & Dateien cachen
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Anfragen abfangen und aus Cache bedienen
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
